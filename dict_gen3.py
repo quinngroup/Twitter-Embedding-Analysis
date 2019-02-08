2f2c6097e2ccc5c629431d279aa6cb31e73904ab
ZanePage <zaneapage@gmail.com>
2019-02-07 22:28:21 -0500

    updating code

--- dict_gen3.py
+++ dict_gen3.py
@@ -15,7 +15,7 @@ twitter_dir = os.listdir(path) # creates list object of all files in path
 
 # gets the file time of a current file for comparison with checkpoint time
 def file_time(file_ind, data_ind):
-    file_path = path + "/" + twitter_dir[ind[file_ind]]
+    file_path = path + "/" + twitter_dir[file_ind]
     with open(file_path) as f:
         data = json.load(f)
         temp_time = datetime.strptime(
@@ -24,21 +24,26 @@ def file_time(file_ind, data_ind):
         return temp_time
 
 # start and end times
-start_time = datetime.datetime(2018,1,22,20,55,1)
-end_time = datetime.datetime(2018,1,29,20,55,1)
+start_time = datetime(2018,1,22,20,55,1)
+end_time = datetime(2018,1,29,20,55,1)
 
-print(star_time.strftime("%Y-%m-%d %H:%M:%S"))
-print(star_time.strftime("%Y-%m-%d %H:%M:%S"))
+print(start_time.strftime("%Y-%m-%d %H:%M:%S"))
+print(end_time.strftime("%Y-%m-%d %H:%M:%S"))
 
 output_file = open("../dicts/2018-01-22.txt", 'w') # file to save first time seg
 print("opened: ", output_file.name) # error checking
 
 for i in range(0, len(twitter_dir)):   # iterates through all files
-    for j in range(0, len(data)):   # iterates through all data in a file
-        # if checkpoint is less then the file time its time for a new time interval
-        # seems like tweets are appended to the top of a file
-        if star_time < file_time(i, j) and end_time > file_time(i,j):
-            print("writing tweet from time:",file_time(i,j))
-            output_file.write(data[j]['tweet_text']) # writes the tweet to file
+
+    file_path = path + "/" + str(twitter_dir[i])
+    with open(file_path) as f:
+      data = json.load(f)
+
+      for j in range(0, len(data)):   # iterates through all data in a file
+          # if checkpoint is less then the file time its time for a new time interval
+          # seems like tweets are appended to the top of a file
+          if start_time < file_time(i, j) and end_time > file_time(i,j):
+              print("writing tweet from time:",file_time(i,j))
+              output_file.write(data[j]['tweet_text']) # writes the tweet to file
 
 output_file.close()
