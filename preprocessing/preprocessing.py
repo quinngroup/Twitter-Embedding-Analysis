aee69a67744051faf3a43354b16be02933ba92d1
Kevin <kevinsadi@live.com>
2020-05-23 20:03:06 -0400

    Creates a panda df per new file

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -34,7 +34,7 @@ def get_data():
 
     return(glob.glob('../../../opt/data/twitter/*'))
 
-def create_data_frame(file_path):
+def create_dataframe(file_path):
     
     '''Creates a dataframe of the filepath passed in.
 
@@ -45,10 +45,9 @@ def create_data_frame(file_path):
 
     '''
     
-    with open(file_path) as json_file:
-        df = pd.read_json(json_file)
-        return df
-      
+    df = pd.read_json(file_path, compression='gzip', lines=True)
+    return df
+    
 def preprocess_tweet(tweet):
     
     '''Does simple preprocessing on the df passed in. Used by clean_dataframe().
@@ -59,17 +58,21 @@ def preprocess_tweet(tweet):
     Returns: a preprocessed tweet
 
     '''
-    
-    #print('Raw tweet: ', tweet, '\n')
-    tweet_without_rt = re.sub('RT', '', tweet)
-    tweet_without_hyperlinks = re.sub(r'https?:\/\.*\/\w*', "", tweet_without_rt)
-    tweet_without_hashtags = re.sub(r'#\w*', '', tweet_without_hyperlinks)
-    # This general process will continue until it's something we like
 
-    return tweet_without_hashtags
+    if isinstance(tweet, float):
+        print('Empty Tweet')
+    else:
+        #print(type(tweet))
+        print('Raw tweet: ', tweet, '\n')
+        tweet_without_rt = re.sub('RT', '', tweet)
+        tweet_without_hyperlinks = re.sub(r'https?:\/\.*\/\w*', "", tweet_without_rt)
+        tweet_without_hashtags = re.sub(r'#\w*', '', tweet_without_hyperlinks)
+        # This general process will continue until it's something we like
+        
+        return tweet_without_hashtags
 
 def tokenize(cleaned_tweet):
-    '''Tokenizes the tweet passed in. Also removes stop words.
+    '''Tokenizes the tweet passed in. Also removes stop words. Used by clean_dataframe().
 
     Keyword arguments:
     cleaned_tweet -- the cleaned tweet.  
@@ -80,7 +83,6 @@ def tokenize(cleaned_tweet):
 
     tweet_tokenizer = TweetTokenizer(preserve_case=False, reduce_len=True, strip_handles=True)
     tweet_list = tweet_tokenizer.tokenize(cleaned_tweet)
-
     tweet_list_no_stopwords = [i for i in tweet_list if i not in english_stopwords]
 
     return tweet_list_no_stopwords
@@ -94,23 +96,12 @@ def clean_dataframe(passed_dataframe):
     passed_dataframe -- the dataframe this wants to be done on
 
     '''
-
-    for tweet in passed_dataframe['tweet_text']:
+    
+    for tweet in passed_dataframe['text']:
         #print('TWEET: ', tweet, '\n')
-        
         print('PREPROCESSED TWEET: ', preprocess_tweet(tweet), '\n')
-        
         #print('TOKENIZED LIST: ', tokenize(preprocess_tweet(tweet)), '\n')
-
-
-########## Sort data by Date (Making dictionary by week (or timespan requested) #################
-
-    ''' We are currently planning on storing dataframes in a single dictionary. Each key is a date and each keyvalue corresponds to that respective
-        date's dataframe.
-    '''
-
-#################################################
-
+        
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Twitter Download Validation',
         epilog = 'lol moar tweetz', add_help = 'How to use',
@@ -125,17 +116,6 @@ if __name__ == "__main__":
     file_list = list(path.glob("*.json.gz"))
     print(f"Found {len(file_list)} files.")
 
-    for x in range(1):
-        current_df = create_data_frame(x)
-        clean_dataframe = clean_dataframe(current_df)
-        print(clean_dataframe)        
-
-    '''
-    # Go through each one and preprocess.
-    out = Parallel(n_jobs = -1)(
-        delayed(examine_file)(f)
-        for f in file_list)
-    
-
-    results = list(reduce(lambda x, y: [x[0] + y[0], x[1] + y[1], x[2] + y[2]], out))
-    '''
+    for x in range(10):
+        current_df = create_dataframe(file_list[x])
+        clean_dataframe(current_df)
