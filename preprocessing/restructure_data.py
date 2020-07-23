4449523ef8ffa63f46b032916786b7b8f39f082d
Kevin <kevinsadi@live.com>
2020-07-22 22:25:07 -0400

    created code to compile all the data in the cluster into one file. still need to add date data. takes a long time to run

--- preprocessing/restructure_data.py
+++ preprocessing/restructure_data.py
@@ -7,6 +7,8 @@ import pandas as pd
 from pandas import DataFrame
 import json
 import gzip
+from pathlib import Path
+import csv
 
 def move_data():
     
@@ -28,51 +30,66 @@ def move_data():
         
         for y in range(10000 * x, (10000 * x) + 10000):
             if y < len(file_list):
-                shutil.copy(file_list[y], dir_name)              
+                shutil.copy(file_list[y], dir_name)      
 
-def make_giant_json(cluster_directory_path):
+def get_files_in_cluster(cluster_path):
+    '''Returns a file_list of all the json files in a cluster.
 
-    '''Creates a giant .json file.
-
-    Key-arguments: cluster_directory_path - the relative path to the cluster directory
-    e.g. ../../reorganized_data/cluster1
-
-    Returns: A dataframe of unprocessed data
+    Keyword arguments:
+    cluster_path -- file path to the cluster. e.g. "../../reorganized_data/cluster1/'
+    
+    Returns: file list of all the json files in a cluster.
 
     '''
-    current_json_dict = dict()
 
-    file_list = new_preprocessing.get_data(cluster_directory_path)
+    path = Path(cluster_path)
+    file_list = list(path.glob("*.json.gz"))
+    return file_list
 
-    for current_file in file_list:
-        with gzip.GzipFile(current_file, 'r') as fil:
-            data_bytes = fil.read()
-            data = json.loads(data_bytes.decode('utf-8'))
-            data.update(current_json_dictionary)
-            fil.seek(0)
-            json.dump(data, fil)
+def load_json(file_path):
+    '''loads the specific file in a cluster into a dataframe. 
 
-    return pd.DataFrame.from_dict(current_json_dict)
+    Keyword arguments:
+    cluster_path -- file path to cluster we are creating 
+    
+    Returns: the unprocessed data df.
 
-def convert_to_csv(cluster_directory_path):
-
-    '''Converts all of the .json files in a cluster directory into a csv file.
-    This happens in the steps, creating a giant dictionary  -> unprocessed df ->
-    processed df -> csv.
-
-    Key-arguments: cluster_directory_path - the relative path to the cluster directory
-    e.g. ../../reorganized_data/cluster1
-
-    Output: a single .csv file found in the directory path this function was given a reference to.
     '''
+    
+    df = pd.read_json(file_path, compression='gzip', lines=True)
+    df = df.dropna(how='all',thresh=4)
+    df = df.reset_index(drop=True)
+    return df
 
-    new_preprocessing.get_data('../../reorganized_data/cluster1')
-    
-    
-    # Get all of the files in a cluster directory path
-    # Add them to a panda df
-    # `pd_df.to_csv(r'cluster_directory_path', index = False, header =True)`
+def preprocess_and_format_df(unprocessed_df, cluster_num):
+    '''This function preprocesses the data found in a specific file and appends the jsons data to it's respective `cluster_data#.txt` if it exists. If not, it creates the file.
+
+    Keyword arguments:
+    unprocessed_df -- the unprocessed dataframe formed by load_json.
+    cluster_num -- which cluster number is this operation being called on
+    '''
+    cluster_txt_file_path = "../../reorganized_data/cluster" + str(cluster_num) + "/cluster" + str(cluster_num) + "files.csv"
+
+    # Creating the base file if it doesn't exist
+    if not os.path.isfile(cluster_txt_file_path):
+        with open(cluster_txt_file_path, 'w') as csvfile:
+            filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            filewriter.writerow(['preprocessed tweet', 'date'])
+            print('made new file')
+
+    with open(cluster_txt_file_path, 'w') as csvfile:
+        basefilewriter =  csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for tweet in unprocessed_df['text']:
+            preprocessed_tweet = ' '.join(preprocessing.preprocess_tweet(tweet))
+            basefilewriter.writerow([preprocessed_tweet])  
+
 
 
 if __name__ == "__main__":
-    make_giant_json('../../reorganized_data/cluster1/')
+    cluster_num = 1 #Change depending on cluster
+    cluster_file_list = get_files_in_cluster("../../reorganized_data/cluster" + str(cluster_num) + "/")
+    
+    # Now for each file, we want to load it into a dataframe, preprocess, and write to a file
+    for file in cluster_file_list:
+        current_unprocessed_df = load_json(file)
+        preprocess_and_format_df(current_unprocessed_df, cluster_num)
