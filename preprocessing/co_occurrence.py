73012496537f4379c2da4e207ba081ccd98164ab
Ivan Mo <imoiwm@gmail.com>
2020-07-30 18:45:40 -0400

    Created the empty co-occurrence matrix.

--- preprocessing/co_occurrence.py
+++ preprocessing/co_occurrence.py
@@ -11,28 +11,33 @@ window = 5
 co_occ_dictionary = {}
 
 for index, row in df.iterrows():
-	alltweets.append(row["preprocessed_text"])
+        alltweets.append(row["preprocessed_text"])
 
 for tweet in alltweets:
-	for word in tweet:
-		if word not in allwords:
-			allwords.append(word)
+        tweet = tweet.replace(']','').replace('[','')
+        tweet = tweet.replace('"','').split(",")
+        for word in tweet:
+                if word not in allwords:
+                        allwords.append(word)
 
 numberofwords = len(allwords)
 
 dictionary = pd.DataFrame(columns=["word"])
 
 for word in allwords:
-	new_row = {"word":word}
-	dictionary = dictionary.append(new_row, ignore_index=True)
+        new_row = {"word":word}
+        dictionary = dictionary.append(new_row, ignore_index=True)
+
+dictionary = dictionary.sort_values(by=['word'])
 
 dictionary = dictionary.set_index('word')
 
-dictionary = dicionary.sort_index(key=lambda x: x.str.lower())
-
 for word in allwords:
-	dictionary[wor] = 0
+        dictionary[word] = 0
 
-dictionary = dictionary.sort_index(inplace=True, axis=1)
+dictionary = dictionary.sort_index(axis=1)
 
 print(dictionary)
+print(":^)")
+
+dictionary.to_csv("../../reorganized_data/cluster1/empty_matrix.csv")
