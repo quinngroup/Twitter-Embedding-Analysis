6514218fe3a1c09395be001c70f37b4dc7b1f9fa
Kevin <kevinsadi@live.com>
2020-08-08 20:47:39 -0400

    Visualization works and added some graphs of it working :D

--- visualization/heatmap.py
+++ visualization/heatmap.py
@@ -1,11 +1,24 @@
-import numpy as np 
+import numpy as np
 import pandas as pd
 import seaborn as sns
+import scipy
+import  matplotlib.pyplot as plt
 
 filled = pd.read_csv('../../reorganized_data/cluster1/filled_matrix.csv')
 filled = filled.loc[:, ~filled.columns.str.contains('^Unnamed')]
+listofwords = filled['word'][0:500].to_list()
+print(listofwords)
+
+print('created list')
 filled = filled.set_index('word')
 filled = filled.reset_index().dropna().set_index('word')
 
-heatmap = sns.heatmap(filled, annot=True, square=True)
-heatmap.savefig('../../reorganized_data/cluster1/heatmap.png')
+sparsematrix = filled.to_numpy()
+shortsparsematrix = sparsematrix[0:500, 0:500]
+print('starting to make heatmap')
+
+plt.subplots(figsize=(100,100))
+heatmap = sns.heatmap(shortsparsematrix, vmax=100, xticklabels = listofwords, yticklabels = listofwords)
+print('finished creating heatmap')
+heatmap.get_figure().savefig('shortsparseheatmap8.png')
+print(':D')
