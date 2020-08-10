64967421a9e7e0a1e2ff648215a49e59a7010928
Kevin <kevinsadi@live.com>
2020-08-10 16:07:40 -0400

    added big graph

--- visualization/heatmap.py
+++ visualization/heatmap.py
@@ -6,7 +6,7 @@ import  matplotlib.pyplot as plt
 
 filled = pd.read_csv('../../reorganized_data/cluster1/filled_matrix.csv')
 filled = filled.loc[:, ~filled.columns.str.contains('^Unnamed')]
-listofwords = filled['word'][0:500].to_list()
+listofwords = filled['word'][0:6000].to_list()
 print(listofwords)
 
 print('created list')
@@ -14,11 +14,11 @@ filled = filled.set_index('word')
 filled = filled.reset_index().dropna().set_index('word')
 
 sparsematrix = filled.to_numpy()
-shortsparsematrix = sparsematrix[0:500, 0:500]
+shortsparsematrix = sparsematrix[0:6000, 0:6000]
 print('starting to make heatmap')
 
 plt.subplots(figsize=(100,100))
 heatmap = sns.heatmap(shortsparsematrix, vmax=100, xticklabels = listofwords, yticklabels = listofwords)
 print('finished creating heatmap')
-heatmap.get_figure().savefig('shortsparseheatmap8.png')
+heatmap.get_figure().savefig('biggestboyshortsparseheatmap9.png')
 print(':D')
