5e9e7e3602d38d32b1d7777d1e129ba80c38664d
imoiwm <imoiwm@gmail.com>
2020-12-03 16:10:34 -0500

    Updated umaptest.py
    Removed some commented-out example code taken from UMAP's basic usage documentation.
    https://umap-learn.readthedocs.io/en/latest/basic_usage.html

--- model/umaptest.py
+++ model/umaptest.py
@@ -6,17 +6,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-#sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
-
-#penguins = pd.read_csv("https://github.com/allisonhorst/palmerpenguins/raw/5b5891f01b52ae26ad8cb9755ec93672f49328a8/data/penguins_size.csv")
-#penguins.head()
-
-#penguins = penguins.dropna()
-#penguins.species_short.value_counts()
-
-#sns.pairplot(penguins, hue='species_short')
-
-
 df = pd.read_csv("../../reorganized_data/cluster1/filled_matrix.csv")
 
 print(df)
