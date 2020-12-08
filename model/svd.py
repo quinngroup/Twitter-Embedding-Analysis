177ad80772fc28069cc5abae12485fed4aa85e92
Ivan Mo <imoiwm@gmail.com>
2020-12-08 17:03:33 -0500

    Attempted to get svd.py up and working on our PPMI matrix.

--- model/svd.py
+++ model/svd.py
@@ -2,11 +2,15 @@ import pandas as pd
 from numpy import linalg
 import numpy as np
 
-df = pd.read_csv('../../reorganized_data/cluster1/filled_matrix.csv')
+df = pd.read_csv('../../reorganized_data/cluster1/ppmi_matrix11.csv')
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 df = df.set_index('word')
+df = df.replace([np.inf,-np.inf],np.nan).fillna(0)
 mat = np.array(df, dtype="float")
 print(mat.shape)
+print(np.isnan(mat).any())
+print(np.isinf(mat).any())
+print(df.dtypes)
 u, s, vh = linalg.svd(mat)
 print(u.shape)
 print(s.shape)
